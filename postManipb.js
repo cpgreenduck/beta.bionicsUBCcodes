00b87e0eaf426af3f559b6c29031379fde599032
cpgreenduck <cpgreenduck@gmail.com>
2020-07-02 21:03:17 -0700

    trying to fix postCatContainer resizing with non javascript compatibility

--- postManipb.js
+++ postManipb.js
@@ -28,6 +28,7 @@ if (is_IE()==false){
 		
 			for (let step1=0;step1<this.postCatSeries.postCats.length;step1++){
 					this.postCatSeries.postCats[step1].classList.remove("expanded");
+					this.postCatSeries.postCats[step1].classList.remove("def");
 		   		this.postCatSeries.postCatHeadlines[step1].classList.remove("active");
 			}
 			this.postCat.classList.add("expanded");
